1740f12032b8bdb1762b0576efad13c0b99f6a30
LeTran <lethanhbaotran.letran@gmail.com>
2025-11-13 21:53:24 +0700

    fix(api): :bug: add Note

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { OrmModule } from './modules/orm.module';
 import { UserController } from './modules/user/user.controller';
 import { UserModule } from './modules/user/user.module';
+import { NoteController } from './modules/note/note.controller';
+import { NoteModule } from './modules/note/note.module';
 
 @Module({
-  imports: [OrmModule, UserModule],
-  controllers: [AppController, UserController],
+  imports: [OrmModule, UserModule, NoteModule],
+  controllers: [AppController, UserController , NoteController],
   providers: [AppService, Logger],
 })
 export class AppModule {}
--- src/modules/orm.module.ts
+++ src/modules/orm.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 import { User } from "../entities/User";
+import { Note } from '../entities/Note';
 
 @Module({
   imports: [
     MikroOrmModule.forRoot(),
     MikroOrmModule.forFeature({
-      entities: [User],
+      entities: [User, Note],
     }),
   ],
   exports: [MikroOrmModule],
